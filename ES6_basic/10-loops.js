d1b39dbc3370f30dbae53edba29553244b7878b8
mpesaran <mahsa.pesaran@gmail.com>
2025-04-02 17:51:05 +1100

    Add other types of for loop

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let [index, value] of array.entries()) {
-    array[index] = appendString + value;
+  let results = []
+  for (let value of array) {
+    results.push(appendString + value);
   }
 
-  return array;
+  return results;
 }
