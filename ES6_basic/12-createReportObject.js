4652dfd09afc418e5481fe7e726d87e4fc3a9032
mpesaran <mahsa.pesaran@gmail.com>
2025-04-02 18:37:10 +1100

    Modify based on lint

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees : {...employeesList,},
+    allEmployees: { ...employeesList },
     getNumberOfDepartments() {
       return Object.keys(this.allEmployees).length;
     },
