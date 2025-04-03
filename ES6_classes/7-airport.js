f4fb66762550be114640a3c82bc9733780793d9c
mpesaran <mahsa.pesaran@gmail.com>
2025-04-03 16:06:22 +1100

    Override toString in the class

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,34 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('name must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new TypeError('code must be a string');
+    }
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
