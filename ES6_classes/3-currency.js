4d66c9e6c41c5333a9dcd1421b457e43686f5a03
mpesaran <mahsa.pesaran@gmail.com>
2025-04-03 12:04:01 +1100

    Add a class with constructor and methos

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new Error('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this.code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this.name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
