a4104b436ff14c35f8e291a5acedd30a8885c783
mpesaran <mahsa.pesaran@gmail.com>
2025-04-03 13:28:06 +1100

    Add an sbstract class and method inside

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.contructor === Building) {
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
       throw new Error("Class is of abstract type and can't be instantiated");
     }
     if (typeof sqft !== 'number') {
