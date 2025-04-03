199a6b824451d5351123c42770a5330d98a17541
mpesaran <mahsa.pesaran@gmail.com>
2025-04-03 14:03:31 +1100

    Add an sbstract class and method inside

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
+    if (new.target === Building) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     if (typeof sqft !== 'number') {
