a24596fe782f1525197f4f154101cd804a83ec43
mpesaran <mahsa.pesaran@gmail.com>
2025-04-03 17:36:59 +1100

    Add a function to class using symbol

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -2,9 +2,9 @@ const CarCloneSymbol = Symbol('cloneCar');
 
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof motor !== 'string' || typeof color !== 'string' || typeof brand !== 'string') {
-      throw new Error('brand, motor, nad color must be string');
-    }
+    // if (typeof motor !== 'string' || typeof color !== 'string' || typeof brand !== 'string') {
+    //   throw new Error('brand, motor, nad color must be string');
+    // }
     this._color = color;
     this._brand = brand;
     this._motor = motor;
