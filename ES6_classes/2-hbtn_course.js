47aaf2b0d4c51749a1de18ce972c62a3cd43d151
mpesaran <mahsa.pesaran@gmail.com>
2025-04-03 11:34:49 +1100

    Add a new class with typed ttrs

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,63 @@
+export default class HolbertonCourse {
+  // Constructor to initialize the object attributes
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      console.error('Name must be a string');
+    } 
+    this._name = name;
+    
+
+    if (typeof length !== 'number') {
+      console.error('Length must be a number');
+    }
+    this._length = length;
+
+    if (!Array.isArray(students) || !students.every(s => typeof s === 'string')) {
+      console.error('Students must be an array of strings');
+    } else {
+      this._students = students;
+    }
+  }
+
+  // Getter for 'name'
+  get name() {
+    return this._name;
+  }
+
+  // Setter for 'name'
+  set name(value) {
+    if (typeof value !== 'string') {
+      console.error('Name must be a string');
+    } 
+      this._name = value;
+    
+  }
+
+  // Getter for 'length'
+  get length() {
+    return this._length;
+  }
+
+  // Setter for 'length'
+  set length(value) {
+    if (typeof value !== 'number') {
+      console.error('Length must be a number');
+    } 
+    this._length = value;
+    
+  }
+
+  // Getter for 'students'
+  get students() {
+    return this._students;
+  }
+
+  // Setter for 'students'
+  set students(value) {
+    if (!Array.isArray(value) || !value.every(s => typeof s === 'string')) {
+      console.error('Students must be an array of strings');
+    } else {
+      this._students = value;
+    }
+  }
+}
