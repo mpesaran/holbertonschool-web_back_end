73ae81271968aa2fb419405583ab9767f29a0ce1
mpesaran <mahsa.pesaran@gmail.com>
2025-04-04 12:16:56 +1100

    Add a functoin to resolve a promise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     if (promise) {
-      resolve({ status: 200, body: 'Success' });
+      resolve({ body: 'success', status: 200 });
     } else {
       reject(new Error(''));
     }
