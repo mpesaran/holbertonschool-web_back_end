754a6d07f2bf896645f446c372e022da7658c0e9
mpesaran <mahsa.pesaran@gmail.com>
2025-04-04 12:20:32 +1100

    Add a functoin to resolve a promise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ body: 'success', status: 200 });
-    } else {
-      reject(Error(''));
-    }
-    console.log('Got a response from the API');
-  });
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
