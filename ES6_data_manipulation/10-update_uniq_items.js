e7b38682b999a41f20ab78bd34e1345b61dbf16d
mpesaran <mahsa.pesaran@gmail.com>
2025-04-04 11:09:33 +1100

    Add a function to change values in a map

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   for (const [item, quantity] of map) {
     if (quantity === 1) {
       map.set(item, 100);
