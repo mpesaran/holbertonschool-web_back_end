35a90ff473a37b084f9b997ca5f29d3e11d8a7da
mpesaran <mahsa.pesaran@gmail.com>
2025-04-04 11:53:57 +1100

    Add a functoin to resolve a promise

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ body: 'Success', status: 200 });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
