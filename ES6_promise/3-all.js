be7797f9e425fd7f969b41d99ce3e4cec0824aff
mpesaran <mahsa.pesaran@gmail.com>
2025-04-04 14:01:35 +1100

    Add a function returns resolved promise with an object

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([
+  return Promise.all([
     uploadPhoto(),
     createUser(),
   ])
-    .then((value) => {
-      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
