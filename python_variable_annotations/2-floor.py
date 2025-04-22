8655cbd94f22ce68a24c5732a4469ad5c8e1a80f
mpesaran <mahsa.pesaran@gmail.com>
2025-04-22 12:08:19 +1000

    Add a function to return floor of a number

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Typed annotation function to floor a float value"""
+import math
+
 def floor(n:float) -> int:
     """floor function will return floor division of n"""
-    return n // 10
+    return math.floor(n)
