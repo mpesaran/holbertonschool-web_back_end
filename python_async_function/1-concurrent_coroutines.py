4f1a17bc292da98c29310f0daa031947436c72b9
mpesaran <mahsa.pesaran@gmail.com>
2025-04-24 12:49:01 +1000

    Add a function to concurrently run a function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Execute multiple coroutines at the same time"""
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """spawns wait_random n times with the specified max_delay"""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     result = await asyncio.gather(*tasks)
     return sorted(result)
-   
