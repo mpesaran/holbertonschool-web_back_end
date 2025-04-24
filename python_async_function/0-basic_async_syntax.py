0edd47378485310729077d0434f4097d65835fe0
mpesaran <mahsa.pesaran@gmail.com>
2025-04-24 12:19:20 +1000

    Add type to function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int =10)-> float:
     """waits for a random seconds less that max_delay and returns it"""
     number = random.uniform(0, max_delay)
     await asyncio.sleep(number)
