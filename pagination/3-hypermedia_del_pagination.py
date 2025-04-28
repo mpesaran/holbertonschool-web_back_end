424e161db14460e04fc7431e669be487f361f926
mpesaran <mahsa.pesaran@gmail.com>
2025-04-29 09:38:22 +1000

    Add a funcction to return hypermedia pagination info

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -39,7 +39,8 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict[str, Any]:
+    def get_hyper_index(
+            self, index: int = None, page_size: int = 10) -> Dict[str, Any]:
         """Return a page of the dataset starting at index with page_size"""
         assert isinstance(index, int) and index >= 0
         assert isinstance(page_size, int) and page_size >= 0
