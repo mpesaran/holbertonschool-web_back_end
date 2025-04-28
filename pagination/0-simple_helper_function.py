50d6ec147c9c8028ab4b6eb9191aa3e30e668df8
mpesaran <mahsa.pesaran@gmail.com>
2025-04-29 09:02:24 +1000

    Add a function to return a page of dataset

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,10 +3,10 @@
 from typing import Tuple
 
 
-def index_range(page:int, page_size:int) -> Tuple[int, int]:
+def index_range(page : int, page_size : int) -> Tuple[int, int]:
     """
     takes two integer arguments page and page_size
-    returns a tuple of size two containing a start index and an end index 
+    returns a tuple of size two containing a start index and an end index
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
