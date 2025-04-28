7e3548446682d4fca2ce92aeedd5a983e168a3ef
mpesaran <mahsa.pesaran@gmail.com>
2025-04-29 08:43:58 +1000

    Add a function to return start and end index

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Pagination"""
+from typing import Tuple
+
+
+def index_range(page:int, page_size:int) -> Tuple[int, int]:
+    """
+    takes two integer arguments page and page_size
+    returns a tuple of size two containing a start index and an end index 
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
