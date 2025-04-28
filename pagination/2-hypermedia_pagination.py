7102dc7488117201267cb7e025031a5fa8d09c55
mpesaran <mahsa.pesaran@gmail.com>
2025-04-29 09:37:19 +1000

    Add a funcction to return hypermedia pagination info

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -49,5 +49,5 @@ class Server:
             'data': data,
             'next_page': page + 1 if page < total_pages else None,
             'prev_page': page - 1 if page > 1 else None,
-            'total_pages':total_pages
+            'total_pages': total_pages
         }
