5d4d801799cf21200cca0b58db4674f656ced933
mpesaran <mahsa.pesaran@gmail.com>
2025-05-01 14:48:20 +1000

    Add a function to return a tuple with str and float values

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Typed annotation function to_kv"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """returns strin representation of a float number"""
+    return tuple(k, float(v**2))
