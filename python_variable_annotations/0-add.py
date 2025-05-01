e8617e555248c0b88af9b724af93c79a695f1ec5
mpesaran <mahsa.pesaran@gmail.com>
2025-05-01 15:04:24 +1000

    Add 2 blank lines at the beginning of the file

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-"""This function is to add two float numbers and will return a float value"""
+"""This function is to add two float numbers and will return
+a float value"""
 
 
 def add(a: float, b: float) -> float:
-    """This function is to add two float numbers and will return a float value"""
+    """This function is to add two float numbers and will
+    return a float value"""
     return a + b
