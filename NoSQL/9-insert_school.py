77c80af984c5cbf7f2f04d7c22cf305cacf52a0f
mpesaran <mahsa.pesaran@gmail.com>
2025-05-01 11:51:21 +1000

    Add a function to insert a doc in a collection

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Use Mongo in a python file"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Add a new document in a collection"""
+    result = mongo_collection.insert(kwargs)
+    return result.inserted_id
