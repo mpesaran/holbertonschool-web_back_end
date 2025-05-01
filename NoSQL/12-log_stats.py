fe900c78fb6be17abe6bed1c748a7283c6628f1d
mpesaran <mahsa.pesaran@gmail.com>
2025-05-01 14:14:10 +1000

    Add a function to return a list of matche ddocument

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-from pymongo import MongoClient
 """Log stats"""
+from pymongo import MongoClient
+
 
 
 def log_stats():
+    """ provides some stats about Nginx logs stored in MongoDB"""
     client = MongoClient("mongodb://127.0.0.1:27017")
     db = client.logs
     collection = db.nginx
