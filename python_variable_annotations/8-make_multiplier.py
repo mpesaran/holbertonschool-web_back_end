ae2e07bf28c1e78bb95eb4590fcb4981206572b6
mpesaran <mahsa.pesaran@gmail.com>
2025-05-01 14:55:02 +1000

    Add a typed function to return a function

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Typed annotation function make_multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """returns a function which multiplies"""
+    def multiply(x: float) -> float:
+        return x * multiplier
+    return multiply    
