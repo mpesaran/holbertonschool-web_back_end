495fe919cb67e5416149c28471b564936035d7c0
mpesaran <mahsa.pesaran@gmail.com>
2025-05-01 15:03:35 +1000

    Add 2 blank lines at the beginning of the file

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """This function is to add two float numbers and will return a float value"""
+
+
 def add(a: float, b: float) -> float:
     """This function is to add two float numbers and will return a float value"""
     return a + b
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Typed annotation function to concat strings"""
+
+
 def concat(str1: str, str2: str) -> str:
     """this function will concat 2 string and return a string"""
     return str1 + str2
