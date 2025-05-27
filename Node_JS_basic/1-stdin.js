e2910a686e8ffd1a8bdcb8b428ce17522c311e6f
mpesaran <mahsa.pesaran@gmail.com>
2025-05-27 14:16:48 +1000

    End stdin before showing end message

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,10 +4,10 @@ process.stdin.setEncoding('utf-8');
 
 process.stdin.on('data', (input) => {
   const name = input.trim();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\r`);
   process.stdin.end();
 });
 
 process.on('exit', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
