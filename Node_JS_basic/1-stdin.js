9761c1d40278d585ff81aab20453e140e0e0e217
mpesaran <mahsa.pesaran@gmail.com>
2025-05-27 14:20:45 +1000

    End stdin before showing end message

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf-8');
-
 process.stdin.on('data', (input) => {
   const name = input.trim();
-  process.stdout.write(`Your name is: ${name}\r`);
-  process.stdin.end();
-});
-
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
 });
