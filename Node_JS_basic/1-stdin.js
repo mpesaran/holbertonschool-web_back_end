9f59e3f57b9ee46e020549a966d3e17e98555814
mpesaran <mahsa.pesaran@gmail.com>
2025-05-27 14:28:32 +1000

    End stdin before showing end message

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,21 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf-8');
+
 process.stdin.on('data', (input) => {
   const name = input.trim();
-  console.log(`Your name is: ${name}\r`);
-  console.log('This important software is now closing');
+  console.log(`Your name is: ${name}`);
+
+  // If input is piped, exit immediately
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+    process.exit();
+  }
+});
+
+// For interactive mode, exit gracefully on Ctrl+D (EOF)
+process.stdin.on('end', () => {
+  if (process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
 });
